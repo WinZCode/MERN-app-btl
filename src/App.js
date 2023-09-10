6feb2615c0e8badf33b9ad52b9babf44b3414c5d
Ngo Thang <nqthang21@gmail.com>
2023-09-10 12:14:47 +0700

    confige route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment } from './redux/slices/counterSlice';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import routes from './routes';
+import DefaultComponent from './components/DefaultComponent/DefaultComponent';
 
-export function App() {
-    const count = useSelector((state) => state.counter.value);
-    const dispatch = useDispatch();
-
+const App = () => {
     return (
         <div>
-            <div>
-                <button
-                    aria-label="Increment value"
-                    onClick={() => dispatch(increment())}
-                >
-                    Increment
-                </button>
-                <span>{count}</span>
-                <button
-                    aria-label="Decrement value"
-                    onClick={() => dispatch(decrement())}
-                >
-                    Decrement
-                </button>
-            </div>
+            <Router>
+                <Routes>
+                    {routes.map((route) => {
+                        const Page = route.page;
+                        const Layout = route.isShowHeader
+                            ? DefaultComponent
+                            : Fragment;
+                        return (
+                            <Route
+                                key={route.path}
+                                path={route.path}
+                                element={
+                                    <>
+                                        <Page />
+                                        <Layout />
+                                    </>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </Router>
         </div>
     );
-}
+};
 
 export default App;
