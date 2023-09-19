443e172fadc69746f163317b55726afae93a8362
Đoàn Đức Thắng <ducthang906@gmail.com>
2023-09-19 19:47:28 +0700

    #19 Tạo API đăng nhập kết hợp jsonwebtoken ở backend

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -30,6 +30,37 @@ const createUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+    try {
+        const {name, email, password, confirmPassword, phone} = req.body
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        if(!name || !email || !password || !confirmPassword || !phone) {
+            return res.status(200).json({
+                status : "ERR",
+                message : 'The input is required'
+            })
+        }else if(!isCheckEmail) {
+            return res.status(200).json({
+                status : "ERR",
+                message : 'The input is email'
+            })
+        }else if (password !== confirmPassword){
+            return res.status(200).json({
+                status : "ERR",
+                message : 'The password is equal confirmPassword'
+            })
+        }
+        console.log("isCheckEmail", isCheckEmail)
+        const response = await UserService.loginUser(req.body); // Sử dụng biến khác cho kết quả của UserService.CreateUser()
+        return res.status(200).json(response); // Sử dụng biến khác cho kết quả
+    } catch (e) {
+        return res.status(404).json({
+            message: e // Sửa lại để truy cập thuộc tính 'message' của đối tượng lỗi
+        });
+    }
+};
+
 module.exports = {
-    createUser, // Sửa tên hàm thành chữ thường theo quy ước của JavaScript
+    createUser,
+    loginUser
 };
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,6 @@
 const User = require("../models/UserModel")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const { genneralAccessToken, genneralRefreshToken } = require("./JwtService");
 
 const createUser = (newUser) => {
     return new Promise(async (resovle, reject) => {
@@ -14,11 +15,12 @@ const createUser = (newUser) => {
                     message : "The email is already"
                 })
             }
+            const hash = bcrypt.hashSync(password, 10)
+            console.log("hash", hash)
             const createdUser = await User.create({
                 name,
                 email,
-                password,
-                confirmPassword,
+                password : hash,
                 phone
             })
             if(createdUser){
@@ -35,6 +37,52 @@ const createUser = (newUser) => {
     });
 };
 
+const loginUser = (userLogin) => {
+    return new Promise(async (resovle, reject) => {
+        const {name, email, password, confirmPassword, phone} = newUser
+        try {
+            const checkUser = await User.findOne({
+                email : email
+            })
+            if(checkUser == null){
+                resovle({
+                    status : "OK",
+                    message : "The user is not defined"
+                })
+            }
+            const comparePassword = bcrypt.compareSync(password, checkUser.password)
+            console.log("comparePassword", comparePassword)
+
+            if(!comparePassword){
+                resovle({
+                    status : "OK",
+                    message : "The password or user is incorrect"
+                })
+            }
+            const access_token = await genneralAccessToken({
+                id : checkUser.id,
+                isAdmin : checkUser.isAdmin
+            })
+
+            const refresh_token = await genneralRefreshToken({
+                id : checkUser.id,
+                isAdmin : checkUser.isAdmin
+            })
+
+            resovle({
+                status : "OK",
+                message : "SUCCESS",
+                access_token,
+                refresh_token
+            })
+            resovle({});
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
 module.exports = {
     createUser,
+    loginUser
 };
